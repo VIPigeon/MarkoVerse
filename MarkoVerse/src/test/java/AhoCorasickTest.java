8d68e016ef3ff8ad1ce7b781000429dac96e6f1d
C1R9A7W2D <DrSpore2@gmail.com>
2025-06-19 17:35:25 +0300

    Add 3 more tests

--- MarkoVerse/src/test/java/AhoCorasickTest.java
+++ MarkoVerse/src/test/java/AhoCorasickTest.java
@@ -12,16 +12,20 @@ public class AhoCorasickTest {
     public void BasicTest() {
         List<String> keywords = Arrays.asList("he", "she", "his", "hers");
 
-        for (int i = 0; i < keywords.size(); i++) {
-            ac.insert(keywords.get(i), i);
-        }
-
-        ac.build();
+        buildACFromList(keywords);
 
         String text = "ushers";
         List<Integer> matches = ac.search(text);
         List<Integer> expectedMatches = List.of(1, 0, 3) ;
         Assert.assertEquals(matches, expectedMatches);
+    }
+
+    private void buildACFromList(List<String> keywords) {
+        for (int i = 0; i < keywords.size(); i++) {
+            ac.insert(keywords.get(i), i);
+        }
+
+        ac.build();
     }
 
     @Test
@@ -36,11 +40,7 @@ public class AhoCorasickTest {
     public void EmptyTextTest() {
         List<String> keywords = Arrays.asList("he", "she", "his", "hers");
 
-        for (int i = 0; i < keywords.size(); i++) {
-            ac.insert(keywords.get(i), i);
-        }
-
-        ac.build();
+        buildACFromList(keywords);
 
         String text = "";
         List<Integer> matches = ac.search(text);
@@ -54,5 +54,46 @@ public class AhoCorasickTest {
         String text = "";
         List<Integer> matches = ac.search(text);
         Assert.assertEquals(List.of(), matches);
+    }
+
+    @Test
+    public void AtTheStartOfTextTest() {
+        List<String> keywords = Arrays.asList("he", "she", "his", "hers", "her");
+
+        buildACFromList(keywords);
+
+        String text = "hersey";
+        List<Integer> matches = ac.search(text);
+        List<Integer> expectedMatches = List.of(0, 4, 3) ;
+        Assert.assertEquals(matches, expectedMatches);
+    }
+
+    @Test
+    public void AtTheEndOfTextTest() {
+        List<String> keywords = Arrays.asList("he", "she", "his", "hers");
+
+        buildACFromList(keywords);
+
+        String text = "porshe";
+        List<Integer> matches = ac.search(text);
+        List<Integer> expectedMatches = List.of(1, 0) ;
+        Assert.assertEquals(matches, expectedMatches);
+    }
+
+    @Test
+    public void MultipleTextsTest() {
+        List<String> keywords = Arrays.asList("he", "she", "his", "hers");
+
+        buildACFromList(keywords);
+
+        List<String> texts = Arrays.asList("ushers", "hersey", "porshe", "this");
+        List<List<Integer>> matches = List.of(List.of(1, 0, 3), List.of(0, 3), List.of(1, 0), List.of(2));
+        for (int i = 0; i < texts.size(); i++) {
+            String text = texts.get(i);
+            List<Integer> expectedMatches = matches.get(i);
+
+            List<Integer> actualMatches = ac.search(text);
+            Assert.assertEquals(actualMatches, expectedMatches);
+        }
     }
 }
