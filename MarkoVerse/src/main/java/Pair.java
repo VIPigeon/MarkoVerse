84224951ff1dea77690342790c120c0d7fd0ed85
VIPigeon <slv.bta@gmail.com>
2025-06-21 13:14:06 +0300

    Добавил пару

---
+++ MarkoVerse/src/main/java/Pair.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Pair<T, S>{
+    T first;
+    S second;
+    public Pair(T first, S second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return  first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
